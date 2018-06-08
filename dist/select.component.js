34a9f23fa8f5ea9a4d8d64cae66be8c31041853d
Mihael Safaric <mihael.safaric@gmail.com>
2018-06-08 11:56:49 +0200

    Build

--- dist/select.component.js
+++ dist/select.component.js
@@ -453,7 +453,8 @@ SelectComponent.decorators = [
                 selector: 'ng-select',
                 template: select_component_html_1.TEMPLATE,
                 styles: [select_component_css_1.STYLE],
-                providers: [exports.SELECT_VALUE_ACCESSOR]
+                providers: [exports.SELECT_VALUE_ACCESSOR],
+                encapsulation: core_1.ViewEncapsulation.None
             },] },
 ];
 /** @nocollapse */
